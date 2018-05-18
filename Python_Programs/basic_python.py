8c9e7c9af046b0de8ba114b20d9c0661e0087b2c
Sunny Thakur <sthakur@qasource.com>
2018-05-18 13:32:01 +0530

    Added demo files

---
+++ Python_Programs/basic_python.py
@@ -0,0 +1,47 @@
+# # Running Code Interactively
+# # Program 1
+# # print ('Hello World'),
+#
+# # Program 2
+# # print (2 ** 8),
+#
+# # Program 3
+# # lumberjack = 'Okay'
+# # print (lumberjack),
+# #
+# # Program 4
+# # print ('Spam! ' * 8)
+#
+# # Import python module
+# # import os
+# # print(os.getcwd())
+# #
+# # For loop
+# # for x in 'spam':
+# #    print (x)
+# #
+# # #For loop 2
+# # for x in 'spam':
+# #    print (x)
+# #    print ('done')
+#
+# # First Program after some tests
+import sys              # Load a library module
+print(sys.platform)
+print(2 ** 100)        # Raise 2 to a power
+x = 'Spam!'
+print(x * 8)           # String repetition
+#input()                # <== ADDED and require to freeze window when execute the py file directly. Otheriwse screen disappear quickly.
+#
+# # Program with define python interpreter;
+# #!C:\Users\sthakur\AppData\Local\Programs\Python\Python36-32\python.exe
+# print('Run', 'away!...') # 3.X function
+
+# Print variables values
+a = 'Orange'
+b = 'Apple'
+c = 'Grapes'
+print(a, b, c)
+
+
+
