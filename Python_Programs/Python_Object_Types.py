4e143a9456393c7fb6d7df419da70f81ae0b375a
Sunny Thakur <sthakur@qasource.com>
2018-05-23 21:52:14 +0530

    Added code to Python_Object_Types.py

---
+++ Python_Programs/Python_Object_Types.py
@@ -0,0 +1,8 @@
+print(123 + 222)    # Integer addition
+print(1.5*4)        # Floating-point multiplication
+print(2**100)       # 2 to the power 100, again
+
+print(len(str(2**1000000)))     # How many digits in a really BIG number?
+
+
+
