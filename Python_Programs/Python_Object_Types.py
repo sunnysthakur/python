fc4d124ea06f35cacd1a6d9568d870220d44552f
Sunny Thakur <sthakur@qasource.com>
2018-05-23 22:03:40 +0530

    Added math module and random module program.

--- Python_Programs/Python_Object_Types.py
+++ Python_Programs/Python_Object_Types.py
@@ -4,5 +4,20 @@ print(2**100)       # 2 to the power 100, again
 
 print(len(str(2**1000000)))     # How many digits in a really BIG number?
 
+print(3.1415*2)     # repr: as code (Pythons < 2.7 and 3.1) & str: user-friendly
+
+#Using inbuilt math module
+import math
+print(math.pi)
+print(math.sqrt(85))
+
+#Using inbuilt random module
+import random
+print(random.random())              #Print random values
+print(random.choice([1, 2, 3, 4]))  #Print random values from list
+
+
+
+
 
 
