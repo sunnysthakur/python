3d92dc1efb2bfd7299b736f5354b4eb17c231ac9
Sunny Thakur <sthakur@qasource.com>
2018-05-24 13:34:45 +0530

    Added Strings Operations code demo

--- Python_Programs/Python_Object_Types.py
+++ Python_Programs/Python_Object_Types.py
@@ -16,6 +16,36 @@ import random
 print(random.random())              #Print random values
 print(random.choice([1, 2, 3, 4]))  #Print random values from list
 
+#String Operations
+S = 'spam'          # Make a 4-character string, and assign it to a name
+print(len(S))       # Length
+
+print(S[0])         # The first item in S, indexing by zero-based position
+print(S[1])         # The second item from the left
+
+print(S[-1])        # The last item from the end in S
+print(S[-2])        # The second-to-last item from the end
+
+print(S[len(S)-1])  # Negative indexing, the hard way
+
+#Slicing in Strings
+print(S)            # A 4-character string
+
+print(S[1:3])       # Slice of S from offsets 1 through 2 (not 3). offsets 1 through 2 (that is, 1 through 3 – 1)
+
+print(S[1:])        # Everything past the first (1:len(S))
+print(S[0:3])       # Everything but the last
+print(S[:3])        # Same as S[0:3]
+print(S[:-1])       # Everything but the last again, but simpler (0:-1)
+print(S[:])          # All of S as a top-level copy (0:len(S))
+
+
+
+
+
+
+
+
 
 
 
