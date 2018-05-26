eec74ea3f48c5c52cb52f2a4b9f853c43ec7ec6c
sunnysthakur <sunnys.thakur@gmail.com>
2018-05-26 23:01:25 +0530

    Fix comments

--- Python_Programs/Python_Object_Types.py
+++ Python_Programs/Python_Object_Types.py
@@ -6,17 +6,17 @@ print(len(str(2**1000000)))     # How many digits in a really BIG number?
 
 print(3.1415*2)     # repr: as code (Pythons < 2.7 and 3.1) & str: user-friendly
 
-#Using inbuilt math module
+# Using inbuilt math module
 import math
 print(math.pi)
 print(math.sqrt(85))
 
-#Using inbuilt random module
+# Using inbuilt random module
 import random
-print(random.random())              #Print random values
-print(random.choice([1, 2, 3, 4]))  #Print random values from list
+print(random.random())              # Print random values
+print(random.choice([1, 2, 3, 4]))  # Print random values from list
 
-#String Operations
+# String Operations
 S = 'spam'          # Make a 4-character string, and assign it to a name
 print(len(S))       # Length
 
@@ -28,7 +28,7 @@ print(S[-2])        # The second-to-last item from the end
 
 print(S[len(S)-1])  # Negative indexing, the hard way
 
-#Slicing in Strings
+# Slicing in Strings
 print(S)            # A 4-character string
 
 print(S[1:3])       # Slice of S from offsets 1 through 2 (not 3). offsets 1 through 2 (that is, 1 through 3 – 1)
@@ -39,17 +39,17 @@ print(S[:3])        # Same as S[0:3]
 print(S[:-1])       # Everything but the last again, but simpler (0:-1)
 print(S[:])          # All of S as a top-level copy (0:len(S))
 
-#Concatenation in Strings
+# Concatenation in Strings
 print(S + 'xyz')            # Concatenation
 print(S * 8)        # Repetition
 
-#Immutability [Numbers, Strings and tupples are immutables where as list and dictionaries isn't
-#S[0] = 'z'  # Immutable objects cannot be changed
+# Immutability [Numbers, Strings and tupples are immutables where as list and dictionaries isn't
+# S[0] = 'z'  # Immutable objects cannot be changed
 T = 'hello'
 T = 'z' + T[1:]     # But we can run expressions to make new objects
 print(T)
 
-#Lists Operations
+# Lists Operations
 U = 'shrubbery'
 L = list(U)         # Expand to a list: [...]
 print(L)
