0d3cc16cf4a6ad6b157fa999dd64e2b81c35a4ab
Sunny Thakur <sthakur@qasource.com>
2018-06-30 14:14:35 +0530

    Added python_for_sysadmins-1 file

---
+++ Learning_Python/python_for_sysadmins-1.py
@@ -0,0 +1,14 @@
+# For condition in Python
+for a in [1, 2]:
+    for b in ['a', 'b']:
+        print(a, b)
+
+# Conditional code
+import os
+if os.path.isdir("C:\TinyTake"):
+    print("TinyTake is a directory")
+else:
+    print("TinyTake is not a directory")
+
+
+
