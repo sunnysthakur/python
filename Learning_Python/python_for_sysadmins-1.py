808da0ceea356a068f528774d4c047b2095c5325
Sunny Thakur <sthakur@qasource.com>
2018-07-02 15:34:02 +0530

    Conditional code

--- Learning_Python/python_for_sysadmins-1.py
+++ Learning_Python/python_for_sysadmins-1.py
@@ -1,14 +1,25 @@
-# For condition in Python
+import sys
+import os
+import subprocess
+
+#For condition in Python
 for a in [1, 2]:
     for b in ['a', 'b']:
         print(a, b)
 
 # Conditional code
-import os
 if os.path.isdir("C:\TinyTake"):
     print("TinyTake is a directory")
 else:
     print("TinyTake is not a directory")
+
+# The Basics
+print("I can program in Python")
+
+# Executing Statements in Python
+print(os.system('exit()'))
+
+
 
 
 
